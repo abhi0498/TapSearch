08ca21cb154a80b1f2491fe0099e1c88410f088d
abhi0498 <randyabhi65@gmail.com>
2019-11-30 21:44:20 +0530

    v1

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,redirect,request
+
+app=Flask(__name__)
+paras=[]
+key=""
+ind=[]
+@app.route("/",methods=["GET","POST"])
+
+def home():
+    if request.method=="GET":
+        return render_template("index.html")
+    else:
+        global paras,key
+        paras=request.form["para"].split("\r\n\r\n")
+        key =request.form["key"]
+        
+
+@app.route("/y",methods=["GET"])
+def y():
+    global ind
+    for i,para in enumerate(paras):
+        if key in para.split():
+            ind.append(i+1)
+    return ' '.join(str(ind))
+
