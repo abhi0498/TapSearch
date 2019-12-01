e85fff3b220a6233d2238aca05d8a7260e2ee3a9
abhi0498 <randyabhi65@gmail.com>
2019-12-01 10:19:48 +0530

    final

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,redirect,request
-
+import PyPDF2
+import re
 app=Flask(__name__)
 paras=[]
 key=""
@@ -12,6 +13,7 @@ def home():
     else:
         global paras,key
         paras=request.form["para"].split("\r\n\r\n")
+       
         i=[j for j in range(len(paras)) if paras[j]=='']
         for d in i:
             paras.remove('')
@@ -22,11 +24,49 @@ def home():
 @app.route("/y",methods=["GET"])
 def y():
     global ind
+    ind=[]
+    new_para=[]
     for i,para in enumerate(paras):
-        if key.lower() in [p.lower() for p in para.split()]:
+        new_para=[p.lower() for p in para.split()]
+        new_para=[re.sub('[^A-Za-z0-9]+', '', p) for p in new_para]
+        if key.lower() in new_para:
             ind.append(i+1)
-    
-    return ' '.join(str(ind))
+    data={
+        'key':key,
+        'ind':ind[:10]
+    }
+    return render_template('results.html',data=data)
+
+@app.route("/pdf",methods=["GET","POST"])
+def pdf():
+    if request.method=="GET":
+        return render_template('pdf.html')
+    else:
+        global paras,key
+        pages=[]
+        f=request.files['files']
+        f.save(f.filename)
+        pdfFileObj = open("family.pdf", 'rb') 
+  
+ 
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+        
+        key =request.form["key"]
+        
+  
+        for i in range(pdfReader.numPages):
+            pages.append(pdfReader.getPage(i))
+        for page in pages:
+            paras.append(page.extractText())
+      
+        
+        
+        return str(pages[0].extractText())
+        
+     
+        
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
